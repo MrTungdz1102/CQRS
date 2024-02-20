dedb6c2d5b45a7e4a8affaa4c8ba3b768839cb33
Tung Dao <daotung492@gmail.com>
2024-02-21 04:46:41 +0700

    integration test

---
+++ CQRS.IntegrationTest/DbContextTest.cs
@@ -0,0 +1,56 @@
+﻿using CQRS.Domain.Models;
+using CQRS.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.IntegrationTest
+{
+    public class DbContextTest
+    {
+        private DbContextOptions<ApplicationDbContext> option;
+        private readonly ApplicationDbContext _db;
+
+        public DbContextTest()
+        {
+            option = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "tempDatabase").Options;
+            _db = new ApplicationDbContext(option);
+        }
+
+        [Fact]
+        public async void Save_SetDateCreatedValue()
+        {
+            var leaveType = new LeaveType
+            {
+                Id = 1,
+                DefaultDay = 10,
+                Name = "Test Vacation"
+            };
+
+            await _db.LeaveTypes.AddAsync(leaveType);
+            await _db.SaveChangesAsync();
+
+            leaveType.DateCreated.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public async void Save_SetDateModifiedValue()
+        {
+            var leaveType = new LeaveType
+            {
+                Id = 1,
+                DefaultDay = 10,
+                Name = "Test Vacation"
+            };
+            await _db.LeaveTypes.AddAsync(leaveType);
+        //    _db.LeaveTypes.Update(leaveType);
+            await _db.SaveChangesAsync();
+
+            leaveType.DateModified.ShouldNotBeNull();
+        }
+    }
+}
